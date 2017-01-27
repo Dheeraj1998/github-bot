a5ffcdef5e80a1e607fd3f9698a588067245e5fe
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2017-01-27 16:56:27 +0530

    Added the code to receive the issue data about every repo.

--- main-bot.py
+++ main-bot.py
@@ -7,8 +7,15 @@ service_url = service_url + user_name + '/repos'
 
 json_data = json.loads(ur.urlopen(service_url).read())
 
-print("\n")
-print("The list of the repos under the user are: ")
+print("\nThe list of the repos & number of unassigned issues under the user are: ")
 
 for x in json_data:
-	print(x["name"])
+	total_issue = 0
+	print(x["name"]," : ")
+	
+	issue_url = 'https://api.github.com/repos/' + user_name + '/' + x["name"] + '/issues?state=all'
+	issue_data = json.loads(ur.urlopen(issue_url).read())
+	
+	for x in issue_data:
+		print(x)
+
