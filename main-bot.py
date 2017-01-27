3ead46497022bb69a97ccd6a490095a16b2d3bc7
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2017-01-27 17:04:25 +0530

    Prints out those repos with unassigned issues.

--- main-bot.py
+++ main-bot.py
@@ -9,13 +9,15 @@ json_data = json.loads(ur.urlopen(service_url).read())
 
 print("\nThe list of the repos & number of unassigned issues under the user are: ")
 
-for x in json_data:
-	total_issue = 0
-	print(x["name"]," : ")
-	
-	issue_url = 'https://api.github.com/repos/' + user_name + '/' + x["name"] + '/issues?state=all'
+for data in json_data:	
+	issue_url = 'https://api.github.com/repos/' + user_name + '/' + data["name"] + '/issues?state=all'
 	issue_data = json.loads(ur.urlopen(issue_url).read())
 	
-	for x in issue_data:
-		print(x)
+	if(issue_data == []):
+		print(data["name"]," : No issues!")
+	
+	else:
+		for issue in issue_data:
+			if (issue["assignee"] == None):
+				print(data["name"]," : Unassigned issues. Hurry now to get assigned!")
 
